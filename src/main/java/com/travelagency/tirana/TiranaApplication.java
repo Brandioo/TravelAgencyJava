4eaf9e75c6aff1bd670917c20c1680ee2215ba8d
ASUS <citozibrand@gmail.com>
2021-05-09 10:56:27 +0200

    Initial commit

---
+++ src/main/java/com/travelagency/tirana/TiranaApplication.java
@@ -0,0 +1,13 @@
+package com.travelagency.tirana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiranaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelagency/tirana/TiranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelagency.tirana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
